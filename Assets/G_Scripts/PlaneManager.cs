661ee314e3f7a5380f22490bed0135e5477001ef
s890218 <wl00882105@gmail.com>
2016-10-02 18:51:04 +0800

    加入腳色控制&場景移動

---
+++ Assets/G_Scripts/PlaneManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneManager : MonoBehaviour {
+
+    private float m_Speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0, 0, -m_Speed * Time.deltaTime*0.5f);
+	}
+}
---
+++ Assets/G_Scripts/PlayerManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    private float m_Speed = 1;
+
+    private bool m_isLeft = false;
+    private bool m_isRight = false;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_isLeft = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            m_isLeft = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_isRight = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            m_isRight = false;
+        }
+
+
+        SetMove();
+    }
+
+    private void SetMove()
+    {
+        if (m_isLeft)
+        {
+            gameObject.transform.position += new Vector3(-0.5f * m_Speed*Time.deltaTime, 0, 0);
+        }
+        if(m_isRight)
+        {
+            gameObject.transform.position += new Vector3(0.5f * m_Speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
