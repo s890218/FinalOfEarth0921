4db2329881ee2870deb719f725ea250fe55e3a62
s890218 <wl00882105@gmail.com>
2016-11-18 20:06:45 +0800

    測試測試

--- Assets/G_Scripts/CreateManager.cs
+++ Assets/G_Scripts/CreateManager.cs
@@ -1,17 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CreateManager : MonoBehaviour {
+public class CreateManager : MonoBehaviour
+{
+
+    public static CreateManager _instance = null;
 
     public GameObject[] m_ArrayCloneObj = null;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void Awake()
+    {
+        _instance = this;
+    }
+
+    //TStaticV.m_NowIndex
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CreateNext()
+    {
+        if (m_ArrayCloneObj == null || m_ArrayCloneObj.Length == 0)
+        {
+            return;
+        }
+
+
+
+    }
+
 }
--- Assets/G_Scripts/TotalStaticValue.cs
+++ Assets/G_Scripts/TotalStaticValue.cs
@@ -1,8 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TStaticV : MonoBehaviour {
+public class TStaticV : MonoBehaviour
+{
 
     public static float m_TotalSpeed = 1;
     public static bool m_GameStart = false;
+    public static int m_NowIndex = 0;   //目前第幾個關卡
+
+    public void Awake()
+    {
+        //初始化
+        m_TotalSpeed = 1;
+        m_NowIndex = 0;
+    }
 }
